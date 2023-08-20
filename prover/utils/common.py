ee118cead7f0f502f1160fe4e0dcefff73c18412
Andrew Milson <andrew.j.milson@gmail.com>
2023-08-20 19:25:40 +1000

    Reduce num_queries in proof params

--- prover/utils/common.py
+++ prover/utils/common.py
@@ -1,3 +1,3 @@
 SANDSTORM = '\\time -f "%E %M" ../sandstorm-mirror/target/release/sandstorm-cli'
 SANDSTORM_PARSER = "../cairo-verifier-utils/target/debug/sandstorm_parser"
-PROOF_PARAMETERS = "--num-queries=24 --lde-blowup-factor=8 --proof-of-work-bits=30 --fri-folding-factor=8 --fri-max-remainder-coeffs=16"
+PROOF_PARAMETERS = "--num-queries=21 --lde-blowup-factor=8 --proof-of-work-bits=33 --fri-folding-factor=8 --fri-max-remainder-coeffs=64"
