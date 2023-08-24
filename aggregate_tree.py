733624f835561b1e41fc2be713de8a392152b01b
Lukas <lucidLuckylee@users.noreply.github.com>
2023-08-24 17:34:31 +0200

    Add BLOCK_HEIGHT_OFFSET to the script

--- aggregate_tree.py
+++ aggregate_tree.py
@@ -1,5 +1,6 @@
 import os
 
+BLOCK_HEIGHT_OFFSET = 100000
 BATCH_SIZE = 1300
 BATCH_NUM = 64 # Has to be power of two
 for i in range(BATCH_NUM):
@@ -7,14 +8,14 @@ for i in range(BATCH_NUM):
     print(batch_cmd)
     #os.system(batch_cmd)
     if i % 2 == 1:
-        aggregate_cmd = f"make aggregate_proof PREV_PROOF=batch_proofs/batch_{i - 1} NEXT_PROOF=batch_proofs/batch_{i} START={(i - 1) * BATCH_SIZE} END={(i + 1) * BATCH_SIZE - 1}"
+        aggregate_cmd = f"make aggregate_proof PREV_PROOF=batch_proofs/batch_{i - 1} NEXT_PROOF=batch_proofs/batch_{i} START={(i - 1) * BATCH_SIZE + BLOCK_HEIGHT_OFFSET} END={(i + 1) * BATCH_SIZE - 1 + BLOCK_HEIGHT_OFFSET}"
         print(aggregate_cmd)
         # os.system(aggregate_cmd)
         j = 4
         while i % j == j - 1:
-            start = (i + 1 - j) * BATCH_SIZE
-            mid = (i + 1 - j // 2) * BATCH_SIZE - 1
-            end = (i + 1) * BATCH_SIZE - 1
+            start = (i + 1 - j) * BATCH_SIZE + BLOCK_HEIGHT_OFFSET
+            mid = (i + 1 - j // 2) * BATCH_SIZE - 1 + BLOCK_HEIGHT_OFFSET
+            end = (i + 1) * BATCH_SIZE - 1 + BLOCK_HEIGHT_OFFSET
             prev_proof = f"aggregate_{start}-{mid}"
             next_proof = f"aggregate_{mid + 1}-{end}"
             aggregate_cmd = f"make aggregate_proof PREV_PROOF={prev_proof} NEXT_PROOF={next_proof} START={start} END={end}"
