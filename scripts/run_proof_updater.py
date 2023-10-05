1c6d869c7d853e3d95dbceab6463ea9a9aa44480
Lukas <lucidLuckylee@users.noreply.github.com>
2023-10-05 23:07:31 +0200

    Add proving command

--- scripts/run_proof_updater.py
+++ scripts/run_proof_updater.py
@@ -1,8 +1,9 @@
 import argparse
+import os
 import time
 from program.src.utils.btc_api import BTCAPI
 
-DEFAULT_BATCH_SIZE = 500
+DEFAULT_BATCH_SIZE = 660
 # How many blocks up to the chain tip have to exist
 CHAINTIP_DELAY = 6
 
@@ -23,6 +24,7 @@ if __name__ == "__main__":
         remaining_blocks = btc_api.get_chain_length() - current_height
         if (remaining_blocks >= batch_size + CHAINTIP_DELAY):
             # TODO: Generate increment proof
+            os.system(f"make BATCH_SIZE={batch_size} PREV_PROOF=increment_0-{current_height} END={current_height + batch_size} increment_proof")
             # TODO: Check proof for failure and send email
             # TODO: Publish new proof via ftp
             print(f"Proving {current_height + batch_size}")
