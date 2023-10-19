3d095eea990fd8f6a76ca89bd4fc5ddfdc135569
mr-abims <adebayoabimbola9@gmail.com>
2023-10-19 12:47:46 +0100

    proof/input update

--- verifier/PkgDemo/index.js
+++ verifier/PkgDemo/index.js
@@ -1,18 +1,21 @@
 import pkg from 'zerosync_header_chain_verifier';
 const { verify } = pkg;
-import fs from 'fs';
-function main() {
-    // Sample serialized public input and proof. You'll replace these with real data.
-    const publicInputBytes = new Uint8Array(fs.readFileSync('air-public-input.json'));
-    const proofBytes = new Uint8Array(fs.readFileSync('proof.bin'));
+import fetch from'node-fetch';
 
+async function main() {
+    const publicInputUrl = 'https://zerosync.org/demo/proofs/latest/air-public-input.json';
+    const proofUrl = 'https://zerosync.org/demo/proofs/latest/aggregated_proof.bin';
+    const publicInputResponse = await fetch(publicInputUrl);
+    const publicInputBytes = new Uint8Array(await publicInputResponse.arrayBuffer());
+    const proofResponse = await fetch(proofUrl);
+    const proofBytes = new Uint8Array(await proofResponse.arrayBuffer());
+
+    // Sample serialized public input and proof. You'll replace these with real data.
+   
     const result = verify(publicInputBytes, proofBytes);
+    console.log(result);
 
     // Assuming the returned result has a property `isValid` to check the verification status
-    if (result.isValid) {
-        console.log("The header chain is valid!");
-    } else {
-        console.log("The header chain is invalid.");
-    }
+    
 }
 main();
